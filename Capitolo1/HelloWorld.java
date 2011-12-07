ee58488bed0dc5ca16a151d0c4424818090f62ae
Alberto Gucciardi <alberto.gucciardi@gmail.com>
2011-12-07 13:46:39 +0100

    The first Soruce app

---
+++ Capitolo1/HelloWorld.java
@@ -0,0 +1,11 @@
+/*****************
+ *HelloWorld.java*
+ *****************/
+ 
+ 
+ public class HelloWorld{
+	public static void main(String arg[]){
+		System.out.println("Hello World");
+		System.exit(0);
+	}
+}
